0354f188e3c7983715f599d0560e653a4f841ac6
mertsunay94@gmail.com <mertsunay94@gmail.com>
2024-01-11 16:54:35 +0300

    dadwd

--- Core/Entites/Concrete/User.cs
+++ Core/Entites/Concrete/User.cs
@@ -6,7 +6,6 @@ namespace Core.Entites.Concrete
     {
         [Key]
         public int Id { get; set; }
-        public int? AccountId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
